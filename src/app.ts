ce3c025773b9063f219118dfb7b8cdc77d614d35
samio11 <samiohasan6@gmail.com>
2025-10-22 08:35:36 +0600

    fix-1

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application, Request, Response } from "express";
+import compression from "compression";
 import cors from "cors";
 import { rootRoute } from "./routes";
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
+app.use(compression());
 
 app.use("/api/v1", rootRoute);
 
